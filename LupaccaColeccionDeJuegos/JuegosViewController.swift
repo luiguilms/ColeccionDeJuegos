efdd4b139473786639383b0644e3b0b2ff85b5a0
Luigui Marco Lupacca Apaza <luigui.lupacca@tecsup.edu.pe>
2023-10-24 17:10:03 -0500

    Imagenes correctamente cargadas y mostradas en table view

---
+++ LupaccaColeccionDeJuegos/JuegosViewController.swift
@@ -0,0 +1,42 @@
+//
+//  JuegosViewController.swift
+//  LupaccaColeccionDeJuegos
+//
+//  Created by Luigui Lupacca on 24/10/23.
+//
+
+import UIKit
+
+class JuegosViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+
+    @IBAction func fotosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true,completion: nil)
+    }
+    @IBAction func camaraTapped(_ sender: Any) {
+    }
+    @IBOutlet weak var JuegoImageView: UIImageView!
+    @IBOutlet weak var tituloTextField: UITextField!
+    @IBAction func agregarTapped(_ sender: Any) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let juego = Juego(context: context)
+        juego.titulo = tituloTextField.text
+        juego.imagen = JuegoImageView.image?.jpegData(compressionQuality: 0.50)
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        navigationController?.popViewController(animated: true)
+    }
+    var imagePicker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagenSeleccionada = info[.originalImage] as? UIImage
+        JuegoImageView.image = imagenSeleccionada
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
